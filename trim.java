c12a89f8e7d136a23efc1a763be758b3e408cb3e
tripathiravi68 <43900031+tripathiravi68@users.noreply.github.com>
2024-09-21 17:01:53 +0530

    analyze the behavior of trim method based on argument

--- trim.java
+++ trim.java
@@ -1,17 +1,52 @@
 import java.util.*;
 public class trim
 {
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the first string");
-		String a=sc.nextLine();
-		String b[]=a.split("l");
-		for(int i=0;i<b.length;i++)
-		System.out.println("the splitted part is "+b[i]);//split karne ke bad usko array me rakhna hota hai
+        public static void main(String args[])
+        {
+                Scanner sc=new Scanner(System.in);
+                System.out.println("enter the first string");
+                String a=sc.nextLine();
+                String b[]=a.split("ram");
+                for(String s: b)
+                {
+                        System.out.println(s+"next");
+                }
+                for(int i=0;i<b.length;i++)
+                System.out.println("the splitted part is--"+i+"---"+b[i]+"\n");//split karne ke bad usko array me rakhna hota hai
 
-	}
+        }
 }
+
+
+
+//used to compare substring of a string within another sstring
+//split returns  String array
+
+
+/*
+enter the first string
+ramlaxmanrambharathramsatrughanramdasrathramramayan
+next
+laxmannext
+bharathnext
+satrughannext
+dasrathnext
+next
+ayannext
+the splitted part is--0---
+
+the splitted part is--1---laxman
+
+the splitted part is--2---bharath
+
+the splitted part is--3---satrughan
+
+the splitted part is--4---dasrath
+
+the splitted part is--5---
+
+the splitted part is--6---ayan
+*/
 
 
 
